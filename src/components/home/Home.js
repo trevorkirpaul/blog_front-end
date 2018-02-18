203d756901cd7ac530c673bb71cb6feec0b452ee
trevorKirpaul <tkirpaul@gmail.com>
2018-02-18 12:53:43 -0500

    Tested API connection, succesfully fetched and rendered content from database

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-export default () => {
-  return (
-    <div>
-      <h1>Trevor Kirpaul</h1>
-      <button>Create Blog Post</button>
-    </div>
-  );
-};
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      blogs: [],
+      error: false,
+    };
+  }
+  componentDidMount() {
+    axios
+      .get('http://localhost:3000/blogs')
+      .then(({ data }) => this.setState({ blogs: [...data] }))
+      .catch(() => this.setState({ error: true }));
+  }
+  render() {
+    const { blogs } = this.state;
+    return (
+      <div>
+        <h1>Blogs</h1>
+        <ul>
+          {blogs && blogs.map(blog => <li key={blog.id}>{blog.title}</li>)}
+        </ul>
+      </div>
+    );
+  }
+}
